bffb64930d6d176dfe7a842960d2fc56d7b21056
Alev Ecevitoglu <45371784+alevecevitoglu@users.noreply.github.com>
2021-02-06 14:44:12 -0500

    Update lab1.m

--- lab1.m
+++ lab1.m
@@ -23,15 +23,18 @@ binsize = 5;
 maxTime = 2000;
 
 subplot(2,1,2); 
-spikeinfo_temp = zeros (1, 2000);
+bin_edges = linspace(0,2000,2000/binsize+1);
+spikeinfo_temp = zeros (1, length(bin_edges)-1);
 for i=1:length(data(x).spks)
     spikeinfo = data(x).spks{1,i};
-    for  j =1:length(spikeinfo) %#ok<ALIGN>
-        somenumber= spikeinfo(j);
-        spikeinfo_temp(somenumber) = spikeinfo_temp(somenumber)+1; 
-	end
+    n = histcounts(spikeinfo,bin_edges);
+    spikeinfo_temp = spikeinfo_temp+n;
 end
-bar(spikeinfo_temp)
+bar(spikeinfo_temp,1) 
+
+bin_centers = bin_edges(1:end-1)+mean(diff(bin_edges))/2;
+bar(bin_centers,spikeinfo_temp,1) %draws the bars at the locations specified by x.
+xlim([0 2000])
 
 xlabel('time[ms]') 
 ylabel('Counts')

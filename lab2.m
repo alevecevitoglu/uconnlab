749ae13c1ce390552a1201b458daeec937133901
Alev Ecevitoglu <45371784+alevecevitoglu@users.noreply.github.com>
2021-02-08 16:05:15 -0500

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -13,10 +13,15 @@ nboot = 10000;
 xdata=data(:,1);
 ydata=data(:,2);
 
-bootstat = bootstrp(nboot,'mean',xdata);
+M = mean(xdata);
+SD = std(xdata);
+
+bootstat = bootstrp(nboot, @mean ,xdata);
+stderror = std(bootstat);
 
 f1=figure;
 histogram(bootstat)
+
 
 %% 2.2 Plot the data
 f2 = figure;
@@ -26,18 +31,18 @@ plot(xdata,ydata, "o") %scatter plot
 %% 2.3 Linear regression
 % linear regression (linear:1st degree!)
 % returns the coefficients for a polynomial p(x) of degree n that is a best fit
-b = polyfit(xdata,ydata,1); %x,y,n n is the degree
+regressionfit = polyfit(xdata,ydata,1); %x,y,n n is the degree
 
 %%make prediction line
 %The polyval function takes x values
 % and parameters as input and returns estimated y values.
 %polyval you can find standard errors for your predictions of y.
-prediction = polyval(b,xdata) %polyval(p,x) evaluates the polynomial p at each point in x. 
+predictionY = polyval(regressionfit, xdata) %polyval(p,x) evaluates the polynomial p at each point in x. 
 
 
 %% 2.4 Plot the data and fit
 f3 = figure;
-plot(xdata,ydata,'o',xdata,prediction)
+plot(xdata,ydata,'o',xdata,predictionY) %hold on
 legend('data', 'linear fit')
 
 
@@ -55,6 +60,26 @@ for i=1:nboot
     bpred(i,:) = polyval(bboot(1,:),x);
 end
 
+
+
+%%
+nboot = 1000;
+%y = linspace(a,b,n) generates a row vector y of n points 
+%linearly spaced between and including a and b
+ 
+[bootstat,bootsam] = bootstrp(nboot,'mean',ydata); %neden ydata
+ 
+x = linspace(min(xdata), max(xdata),100); 
+for i=1:nboot
+    bboot(i,:) = polyfit(data(bootsam(:,i),1), data(bootsam(:,i),2),1);
+    bpred(i,:) = polyval(bboot(1,:),x);
+end
+ 
+
+bpred = bpred(1,:)';
+plot(x,bpred,'k');
+
+%% check: http://www.mbfys.ru.nl/~robvdw/DGCN22/PRACTICUM_2011/LABS_2011/ALTERNATIVE_LABS/Lesson_6.html
 
 
 

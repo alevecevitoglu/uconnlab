9694cca670b1ad9d337f0c1432600553f6f77591
Alev Ecevitoglu <45371784+alevecevitoglu@users.noreply.github.com>
2021-02-10 14:29:23 -0500

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -1,5 +1,5 @@
 %% 2.0 Load data...
-
+close all
 % either Axon Diameter vs Conduction Velocity
 %data = readtable('Hursh1936.csv');
 
@@ -8,7 +8,6 @@ data = readtable('BodyBrain.csv');
 data = table2array(data);
 
 %% 2.1 Bootstrap to estimate the mean and sem of the x-variable
-
 nboot = 10000;
 xdata=data(:,1);
 ydata=data(:,2);
@@ -31,72 +30,31 @@ plot(xdata,ydata, "o") %scatter plot
 %% 2.3 Linear regression
 % linear regression (linear:1st degree!)
 % returns the coefficients for a polynomial p(x) of degree n that is a best fit
-regressionfit = polyfit(xdata,ydata,1); %x,y,n n is the degree
-
-%%make prediction line
-%The polyval function takes x values
-% and parameters as input and returns estimated y values.
-%polyval you can find standard errors for your predictions of y.
-predictionY = polyval(regressionfit, xdata) %polyval(p,x) evaluates the polynomial p at each point in x. 
-
+p_coef = polyfit(xdata,ydata,1); %x,y,n n is the degree
 
 %% 2.4 Plot the data and fit
 f3 = figure;
-plot(xdata,ydata,'o',xdata,predictionY) %hold on
+hold on
+x = linspace(min(xdata), max(xdata),100); %all xdata points, devided to 100
+predictionY = polyval(p_coef, x); %polyval(p,x) evaluates the polynomial p at each point in x. 
+plot(xdata,ydata,'o',x,predictionY,'-') 
 legend('data', 'linear fit')
 
 
 %% 2.5 Bootstrap to find error-bars for the model parameters
 nboot = 1000;
-%y = linspace(a,b,n) generates a row vector y of n points 
-%linearly spaced between and including a and b
-
-[bootstat,bootsam] = bootstrp(nboot,'mean',ydata); %neden ydata
-
-x = linspace(min(xdata), max(xdata),100); 
-for i=1:nboot
-    bboot(i,:) = polyfit(data(bootsam(:,1), 
-                            data(bootsam(:,i),2),1)
-    bpred(i,:) = polyval(bboot(1,:),x);
-end
-
-
-
-%%
-nboot = 1000;
-%y = linspace(a,b,n) generates a row vector y of n points 
-%linearly spaced between and including a and b
+[bootstat,bootsam] = bootstrp(nboot,'mean',ydata); %ydata is bootstrapped, bootsam gives all the possibilities
  
-[bootstat,bootsam] = bootstrp(nboot,'mean',ydata); %neden ydata
- 
-x = linspace(min(xdata), max(xdata),100); 
 for i=1:nboot
-    bboot(i,:) = polyfit(data(bootsam(:,i),1), data(bootsam(:,i),2),1);
-    bpred(i,:) = polyval(bboot(1,:),x);
+    bboot(i,:) = polyfit(data(bootsam(:,i),1), data(bootsam(:,i),2),1); 
+    %returns all 1000 bootstrap samples and make a matrix for each p_coef
+    bpred(i,:) = polyval(bboot(i,:),x); %returns the y-values for each coef
 end
  
+err = 1.96.*std(bpred); %of predicted ydata
 
-bpred = bpred(1,:)';
-plot(x,bpred,'k');
-
-%%
-
-nboot = 1000;
-%y = linspace(a,b,n) generates a row vector y of n points 
-%linearly spaced between and including a and b
- 
-[bootstat,bootsam] = bootstrp(nboot,'mean',ydata);
- 
-x = linspace(min(xdata), max(xdata),100); 
-for i=1:nboot
-    bboot(i,:) = polyfit(data(bootsam(:,i),1), data(bootsam(:,i),2),2);
-    bpred(i,:) = polyval(bboot(i,:),x);
-end
- 
-f4 = figure;
-plot(x,bpred,'--');
+plot(x,predictionY+err,'k--') %CI calculation
+plot(x,predictionY-err,'k--') %CI calculation
 hold off
-
-
 
 
